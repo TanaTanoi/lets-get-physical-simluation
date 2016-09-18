fc5de2e7d5050dde1edbac814ae8bf9119c81b4b
TanaTanoi <tana.tanoi@gmail.com>
2016-09-18 18:54:43 +1200

    Add basic mouse controls for rotation and background
    Set background to be half and half

--- test.py
+++ test.py
@@ -11,7 +11,12 @@ import math
 name = 'ball_glut'
 class Renderer:
     def __init__(self, filename):
-        self.c = -10.
+        # m_ is the mouse prefix
+        self.m_x = 0
+        self.m_y = 0
+        self.m_down = False
+        # r_ is the rotations prefix
+        self.r_x = 0
         self.z = 5.
         values = offfile.OffFile(filename)
         values = values.read_file()
@@ -26,12 +31,12 @@ class Renderer:
         glutInitWindowSize(400,400)
         glutCreateWindow(name)
 
-        glClearColor(0.,0.,0.,1.)
+        glClearColor(0.99,0.99,0.99,1.)
         glShadeModel(GL_SMOOTH)
         glEnable(GL_CULL_FACE)
         glEnable(GL_DEPTH_TEST)
         glEnable(GL_LIGHTING)
-        lightZeroPosition = [10.,4.,10.,1.]
+        lightZeroPosition = [0.,10.,0.,1.]
         lightZeroColor = [0.8,1.0,0.8,1.0] #green tinged
         glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
         glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
@@ -40,6 +45,8 @@ class Renderer:
         glEnable(GL_LIGHT0)
         glutDisplayFunc(self.display)
         glutKeyboardFunc(self.keyboard)
+        glutMouseFunc(self.mouse_input)
+        glutMotionFunc(self.mouse_motion)
         glutIdleFunc(self.display)
         glMatrixMode(GL_PROJECTION)
         gluPerspective(40.,1.,1.,40.)
@@ -58,21 +65,33 @@ class Renderer:
         if(char == '\x1b' or char == 'q'):
             exit()
         elif(char == 'w'):
-            self.z -= 2
+            self.z -= 1
         elif(char == 's'):
-            self.z += 2
+            self.z += 1
+
+    def mouse_input(self, button, state, x, y):
+        print("MOUSE ", button, " state: ", state, " x: ", x, " y: ", y)
+        self.m_x = x
+        self.m_y = y
+        self.m_down = state == 0
+
+    def mouse_motion(self, x, y):
+        print("DRAG x: ", x,  " y: ",y )
+        if(self.m_down):
+            self.r_x += x - self.m_x
+            self.m_x = x
 
     def display(self):
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        self.draw_background()
+        glMatrixMode(GL_MODELVIEW)
         glPushMatrix()
 
         glTranslatef(0, 0, self.z)
-        glRotatef(self.c * 1000, 0, 1, 0)
+        glRotatef(self.r_x, 0, 1, 0)
         glRotatef(90, -1, 0, 0)
-        self.c += 0.001
-        color = [1.0,0.,0.,1.]
+        color = [0.7,0.0,0.5,1.]
         glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
-        # glutSolidSphere(2,20,20)
         self.draw_object()
         glPopMatrix()
         glutSwapBuffers()
@@ -96,6 +115,33 @@ class Renderer:
         for vert_id in range(len(self.verts)):
             self.verts[vert_id] += middle_point
 
+
+    def draw_background(self):
+        glMatrixMode(GL_PROJECTION)
+        glClear(GL_DEPTH_BUFFER_BIT)
+        glShadeModel(GL_FLAT)
+        glDisable(GL_DEPTH_TEST)
+        glPushMatrix()
+        glLoadIdentity()
+        w = glutGet(GLUT_WINDOW_WIDTH)
+        h = glutGet(GLUT_WINDOW_HEIGHT)
+        gluOrtho2D(0, w, h, 0)
+        glMatrixMode(GL_MODELVIEW)
+        glPushMatrix()
+        glLoadIdentity()
+        color = [0.99,0.99,0.99,1.]
+        glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
+        glMaterialfv(GL_FRONT,GL_AMBIENT,1)
+        glBegin(GL_QUADS);
+        glVertex2f(0, 0)
+        glVertex2f(0, h / 2.)
+        glVertex2f(w, h / 2.)
+        glVertex2f(w, 0)
+        glEnd();
+        glPopMatrix()
+        glMatrixMode(GL_PROJECTION)
+        glPopMatrix()
+        glEnable(GL_DEPTH_TEST)
 # if __name__ == '__main__': main()
-r = Renderer("cactus_small.off")
+r = Renderer("data/cactus_small.off")
 r.main()
