16f6d4180557ceb739b5fff554b11f58bf6ccc61
TanaTanoi <tana.tanoi@gmail.com>
2016-09-19 23:32:12 +1200

    Changed from pyopengl to pyglet
    Also added texturing to the flag

--- renderer.py
+++ renderer.py
@@ -1,72 +1,66 @@
-from OpenGL.GLUT import *
-from OpenGL.GLU import *
-from OpenGL.GL import *
+import pyglet
+from pyglet.window import key
+from pyglet.gl import *
 import sys
 import offfile
 import face
 import numpy as np
 import math
 import model
+from PIL import Image
 
-name = 'ball_glut'
 class Renderer:
-    def __init__(self, filename):
+    def __init__(self, filename=""):
         # m_ is the mouse prefix
         self.m_x = 0
         self.m_y = 0
         self.m_down = False
         # r_ is the rotations prefix
         self.r_x = 0
-        self.z = 5.
-        values = offfile.OffFile(filename)
-        values = values.read_file()
-        self.model = model.Model(values[0], values[1])
-        self.model.center()
+        self.z = 0
+        if len(filename) > 0:
+            values = offfile.OffFile(filename)
+            values = values.read_file()
+            self.model = model.Model(values[0], values[1])
+            self.model.center()
+        else:
+            self.model = model.Model.generate_plane(50, 30)
+            self.model.center()
 
     def main(self):
-        # self.c = 0
-        glutInit(sys.argv)
-        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-        glutInitWindowSize(400,400)
-        glutCreateWindow(name)
+        self.window = pyglet.window.Window()
+        self.initialize_window_callbacks()
 
         glClearColor(0.8,0.8,0.8,1.)
         glShadeModel(GL_SMOOTH)
-        glEnable(GL_CULL_FACE)
         glEnable(GL_DEPTH_TEST)
-        glEnable(GL_LIGHTING)
-        lightZeroPosition = [0.,10.,0.,1.]
-        lightZeroColor = [0.8,1.0,0.8,1.0] #green tinged
-        glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
-        glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
-        glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
-        glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
-        glEnable(GL_LIGHT0)
-        glutDisplayFunc(self.display)
-        glutKeyboardFunc(self.keyboard)
-        glutMouseFunc(self.mouse_input)
-        glutMotionFunc(self.mouse_motion)
-        glutIdleFunc(self.display)
-        glMatrixMode(GL_PROJECTION)
-        gluPerspective(40.,1.,1.,40.)
+        # glEnable(GL_LIGHTING)
+        # lightZeroPosition = Renderer.opengl_array([0.,10.,0.,1.])
+        # lightZeroColor = Renderer.opengl_array([0.8,1.0,0.8,1.0]) #green tinged
+        # glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
+        # glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
+        # glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
+        # glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
+        # glEnable(GL_LIGHT0)
+        # glMatrixMode(GL_PROJECTION)
+        # gluPerspective(40.,1.,1.,40.)
+        # gluLookAt(0,0,-10,
+        #           0,0,0,
+        #           0,1,0)
+        glMatrixMode(gl.GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(60., self.window.width / self.window.height, 1, 100.)
         glMatrixMode(GL_MODELVIEW)
-        gluLookAt(0,0,10,
-                  0,0,0,
-                  0,1,0)
         glPushMatrix()
-        glutMainLoop()
+        textures = []
+        self.load_texture("doggerflag.png", textures)
+
+        pyglet.app.run()
         return
 
-    def keyboard(self, key, x, y):
-        print("KEYBOARD ", key, " x: ",x, " y: ", y)
-        char = key.decode("utf-8")
-        # escape key
-        if(char == '\x1b' or char == 'q'):
-            exit()
-        elif(char == 'w'):
-            self.z -= 1
-        elif(char == 's'):
-            self.z += 1
+    def initialize_window_callbacks(self):
+        self.window.on_draw = self.on_draw
+        self.window.on_key_press = self.on_key_press
 
     def mouse_input(self, button, state, x, y):
         print("MOUSE ", button, " state: ", state, " x: ", x, " y: ", y)
@@ -80,30 +74,46 @@ class Renderer:
             self.r_x += x - self.m_x
             self.m_x = x
 
-    def display(self):
+    def on_draw(self):
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        self.draw_background()
-        glMatrixMode(GL_MODELVIEW)
         glPushMatrix()
 
-        glTranslatef(0, 0, self.z)
+        glTranslatef((self.window.width / 2), self.window.height / 2, self.z)
         glRotatef(self.r_x, 0, 1, 0)
-        glRotatef(90, -1, 0, 0)
-        color = [0.7,0.0,0.5,1.]
-        glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
         self.draw_object()
         glPopMatrix()
-        glutSwapBuffers()
-        return
 
     def draw_object(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+
         glBegin(GL_TRIANGLES)
         for face in self.model.faces:
             for v_id in face.vertex_ids():
                 vert = self.model.verts[v_id]
+                uv = self.model.uvs[v_id]
+                glTexCoord2f(uv[0], uv[1])
                 glVertex3f(vert[0], vert[1], vert[2])
+
         glEnd()
 
+    def draw_lines(self):
+        glBegin(GL_LINES)
+        for x in range(0, self.window.width, 10):
+            for y in range(0, self.window.height, 10):
+                glVertex3f(x, y, -x / 100)
+        glEnd()
+
+    def on_key_press(self, symbol, modifiers):
+        if(symbol == key.A):
+            self.r_x += 0.1
+        elif(symbol == key.D):
+            self.r_x -= 0.1
+        elif(symbol == key.S):
+            self.z -= 2
+        elif(symbol == key.W):
+            self.z += 2
+        elif(symbol == key.Q):
+            self.r_x += 180
 
     def draw_background(self):
         glMatrixMode(GL_PROJECTION)
@@ -112,15 +122,15 @@ class Renderer:
         glDisable(GL_DEPTH_TEST)
         glPushMatrix()
         glLoadIdentity()
-        w = glutGet(GLUT_WINDOW_WIDTH)
-        h = glutGet(GLUT_WINDOW_HEIGHT)
+        w = self.window.width
+        h = self.window.height
         gluOrtho2D(0, w, h, 0)
         glMatrixMode(GL_MODELVIEW)
         glPushMatrix()
         glLoadIdentity()
-        color = [0.99,0.99,0.99,1.]
+        color = Renderer.opengl_array([0.99,0.99,0.99,1.])
         glMaterialfv(GL_FRONT, GL_DIFFUSE, color)
-        glMaterialfv(GL_FRONT, GL_AMBIENT, 1)
+        glMaterialfv(GL_FRONT, GL_AMBIENT, 1.)
         glBegin(GL_QUADS);
         glVertex2f(0, 0)
         glVertex2f(0, h / 2.)
@@ -131,6 +141,21 @@ class Renderer:
         glMatrixMode(GL_PROJECTION)
         glPopMatrix()
         glEnable(GL_DEPTH_TEST)
+
+    def load_texture(self, filename, textures):
+        image = pyglet.image.load(filename)
+        textures.append(image.get_texture())
+
+        glEnable(textures[-1].target)
+        glBindTexture(textures[-1].target, textures[-1].id)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height,
+        0, GL_RGBA, GL_UNSIGNED_BYTE,
+        image.get_image_data().get_data('RGBA',
+        image.width * 4))
+
+    def opengl_array(array):
+        return (GLfloat * len(array))(*array)
+
 # if __name__ == '__main__': main()
-r = Renderer("data/cactus_small.off")
+r = Renderer()
 r.main()
